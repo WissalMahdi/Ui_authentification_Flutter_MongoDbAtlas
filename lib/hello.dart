22710c429c8efef424cc81c7b7882adbd1c958cb
WissalMahdi <76208246+WissalMahdi@users.noreply.github.com>
2022-03-04 21:42:58 +0100

    commit

---
+++ lib/hello.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HELLO extends StatelessWidget {
+  const HELLO({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/hi.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HI extends StatefulWidget {
+  const HI({Key? key}) : super(key: key);
+
+  @override
+  State<HI> createState() => _HIState();
+}
+
+class _HIState extends State<HI> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
